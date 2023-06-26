76afd5d64007dbf9c68e801c8982719a8a68bd8b
Salvador Cirino Castrovinci <salvadorcirino2004@hotmail.com>
2023-06-26 08:41:15 -0300

    update menu

---
+++ Canon_VsCanon/Canon_VsCanon/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Canon_VsCanon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.comenzarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.finalizarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.finalizarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.creditosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.comenzarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(973, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // comenzarToolStripMenuItem
+            // 
+            this.comenzarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.finalizarToolStripMenuItem,
+            this.finalizarToolStripMenuItem1,
+            this.creditosToolStripMenuItem});
+            this.comenzarToolStripMenuItem.Name = "comenzarToolStripMenuItem";
+            this.comenzarToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.comenzarToolStripMenuItem.Text = "Menu";
+            // 
+            // finalizarToolStripMenuItem
+            // 
+            this.finalizarToolStripMenuItem.Name = "finalizarToolStripMenuItem";
+            this.finalizarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.finalizarToolStripMenuItem.Text = "Comenzar";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // finalizarToolStripMenuItem1
+            // 
+            this.finalizarToolStripMenuItem1.Name = "finalizarToolStripMenuItem1";
+            this.finalizarToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.finalizarToolStripMenuItem1.Text = "Finalizar";
+            // 
+            // creditosToolStripMenuItem
+            // 
+            this.creditosToolStripMenuItem.Name = "creditosToolStripMenuItem";
+            this.creditosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.creditosToolStripMenuItem.Text = "Creditos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 500);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Cañon Vs Cañon";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem comenzarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem finalizarToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem finalizarToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem creditosToolStripMenuItem;
+    }
+}
+
